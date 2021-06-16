624389ffc7e4660c06834f73cb5462fa4deab1f4
HashEngineering <hashengineeringsolutions@gmail.com>
2021-06-16 11:09:16 -0400

    build: include c++_shared libs

--- dashj-merk/android_build.sh
+++ dashj-merk/android_build.sh
@@ -26,3 +26,8 @@ cp ./src/main/rust/target/aarch64-linux-android/release/${libName} ${jniLibs}/ar
 cp ./src/main/rust/target/armv7-linux-androideabi/release/${libName} ${jniLibs}/armeabi-v7a/${libName}
 cp ./src/main/rust/target/i686-linux-android/release/${libName} ${jniLibs}/x86/${libName}
 cp ./src/main/rust/target/x86_64-linux-android/release/${libName} ${jniLibs}/x86_64/${libName}
+
+cp -v $ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/arm64-v8a/libc++_shared.so ${jniLibs}/arm64-v8a/
+cp -v $ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/x86_64/libc++_shared.so ${jniLibs}/x86_64/
+cp -v $ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so ${jniLibs}/armeabi-v7a/
+cp -v $ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/x86/libc++_shared.so ${jniLibs}/x86/
